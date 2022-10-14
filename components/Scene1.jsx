98d8186ced29d258d4c100e6c7ad9b913fdfa513
Lucas S D Frota <93730871+LuckDays@users.noreply.github.com>
2022-10-14 10:03:43 -0300

    Better

--- components/Scene1.jsx
+++ components/Scene1.jsx
@@ -7,6 +7,7 @@ import {
   RoundedBox,
   OrbitControls,
 } from "@react-three/drei";
+import { SocialIcon } from "react-social-icons";
 import { useRef } from "react";
 
 function Bola({ position, color, radius }) {
@@ -107,13 +108,27 @@ function ThreeScene() {
       />
       {/* Banner Explicativo */}
       <HtmlContent position={[-0.6, 0.3, 3]}>
-        <div className='h-[600px] w-[1000px] flex items-center justify-center bg-white rounded-xl backdrop-blur-xl bg-opacity-50 px-5'>
-          <p className='text-4xl'>
-            Hello,
-            <br />
-            <br /> in this section just some primitive shapes movin' around.
-            <br /> from drei.
-          </p>
+        <div className='select-none h-[600px] w-[1000px] grid grid-rows-2 items-center justify-center bg-white rounded-xl backdrop-blur-xl bg-opacity-50 px-5'>
+          <div className='flex flex-col row-span-1 w-full h-full items-center justify-center'>
+            <p className='text-4xl'>
+              Hello,
+              <br />
+              <br /> in this section just some primitive shapes movin' around.
+              <br /> from drei.
+              <p>and this is a real HTML content as well</p>
+            </p>
+          </div>
+          <div className='flex flex-col row-span-1 w-full px-10 gap-10 h-full mt-10  items-center justify-center'>
+            <p className='text-3xl'>Social n' contact</p>
+            <div className='flex justify-center p-4 gap-5 rounded-full w-[200px] bg-white relative'>
+              <span className='hover:-translate-y-4 hover:scale-125 transition-all duration-300 ease-in-out'>
+                <SocialIcon url='https://www.linkedin.com/in/lucas-dias-a3808821b/' />
+              </span>
+              <span className='hover:-translate-y-4 hover:scale-125 transition-all duration-300 ease-in-out'>
+                <SocialIcon url='https://t.me/LuckCoder' />
+              </span>
+            </div>
+          </div>
         </div>
       </HtmlContent>
       <Bola radius={1.4} position={[4, 3, -5]} color='black' />
